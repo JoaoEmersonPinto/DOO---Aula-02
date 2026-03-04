de01c1f6dc700853665b7c1af5b02d5a09417ff0
JoaoEmersonPinto <jepinto@minha.fag.edu.br>
2026-03-04 19:40:57 -0300

    Second commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        saudar();
+    }
+
+
+    public static void saudar() {
+        System.out.println("Hello World!");
+    }
+
+
+}
+
+
