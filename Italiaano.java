bba4e08b03786f45fb89ff4ea022fbd0d6260778
JoaoEmersonPinto <jepinto@minha.fag.edu.br>
2026-03-04 19:57:56 -0300

    Alterado arquivo Italiaano

--- Italiaano.java
+++ Italiaano.java
@@ -3,7 +3,7 @@ public class Italiaano {
     public static void main(String[] args) {
         saudar();
     }
-
+    
     public static void saudar() {
         System.out.println("Ciao Mondo!");
     }
